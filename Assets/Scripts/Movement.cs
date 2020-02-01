9a5fa3c18778758164ad494951fbb382b66dea36
Ben Leech <b.leech@hotmail.ca>
2020-02-01 00:20:46 -0600

    got basic movement working

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
-{
+public class Movement : MonoBehaviour {
+
+    public float movementSpeed = 1f;
+    new private Rigidbody2D rigidbody;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate() {
+        Vector2 veloctiy = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+        rigidbody.MovePosition(rigidbody.position + veloctiy * Time.fixedDeltaTime * movementSpeed);
+    }
+
+    void InputControl() {
+        if(Input.GetAxis("vertical") != 0) {
+
+        }
+
+        if(Input.GetAxis("horizontal") != 0) {
+
+        }
     }
 }
