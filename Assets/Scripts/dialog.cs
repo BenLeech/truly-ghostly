ac274873807a2b215a82bb894bdba92447391abd
Laurie Harding-Russell <laurieHardingRussell@gmail.com>
2020-02-01 10:45:45 -0600

    We displayed text.

---
+++ Assets/Scripts/dialog.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class dialog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newText = new GameObject("text", typeof(RectTransform));
+        
+        TextMesh myText = newText.AddComponent<TextMesh>();
+        myText.text = "We did it!";
+
+
+        // var newTextComp = newText.AddComponent<Text>();
+        // newTextComp.text = "test";
+        // newTextComp.font;
+        // newTextComp.color = color;
+        // newTextComp.fontSize = 16;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
